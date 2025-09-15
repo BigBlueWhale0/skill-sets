363faa046afc3b1afb012880334040b5e69dbc29
bigbluewhale0 <meetjoeblack0@gmail.com>
2025-09-15 17:04:58 +0300

    Add includes and pages

--- web/intergalactic-cats/app.py
+++ web/intergalactic-cats/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import requests
 import pycountry
+import json
 
 
 app = Flask(__name__)
@@ -16,11 +17,27 @@ def get_location_data():
     }
     return location_data
 
+def get_news():
+    with open("data/news.json") as file:
+        content = json.load(file)
+    return content
+
+
 @app.route("/")
 def home():
     location_data = get_location_data()
     flag = pycountry.countries.get(alpha_2=location_data["country"]).flag
     return render_template("index.html", country=flag, city=location_data["city"])
+
+@app.route("/news")
+def news():
+    all_posts = get_news()
+    print(all_posts)
+    return render_template("news.html", all_posts=all_posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
