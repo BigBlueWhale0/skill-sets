23b43c4449a36689243414d601ad042952512c69
bigbluewhale0 <meetjoeblack0@gmail.com>
2025-10-06 22:07:37 +0300

    Add blocks and auth

--- web/articles_portal/articles_portal/settings.py
+++ web/articles_portal/articles_portal/settings.py
@@ -118,6 +118,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = "home" 
+LOGOUT_REDIRECT_URL = "login"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
